4778aa6f50c43160d2a2193f358d3dda78defe3d
MaminSiberian <120947225+MaminSiberian@users.noreply.github.com>
2024-01-22 17:54:55 +0300

    Update PictureRequestButton.cs

--- Assets/GameResources/Features/WebRequest/Scripts/PictureRequestButton.cs
+++ Assets/GameResources/Features/WebRequest/Scripts/PictureRequestButton.cs
@@ -2,6 +2,9 @@ namespace Task1.Features.WebRequest
 {
     using UnityEngine;
 
+    /// <summary>
+    /// Кнопка запроса картинки с сервера
+    /// </summary>
     public class PictureRequestButton : AbstractButton
     {
         [SerializeField]
